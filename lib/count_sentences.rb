4cb43507b31429dc19888dd5ae22e3587fb23212
gyeon <gyeon19@gmail.com>
2016-06-14 15:46:09 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")
+  
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/(?<=[!?.])/).reject {|string| string.length < 2}.count
+    # self.reject {|string| self.length < 3}
+    # self.reject {|item| item == " "}
   end
 end
+
+#call enumerables on collections - arrays or hashes
